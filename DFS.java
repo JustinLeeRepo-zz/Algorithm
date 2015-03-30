befce3fc7c1b41f38e6cf616a45b158c153558f3
Justin Lee <justinj.lee23@gmail.com>
2015-03-29 23:26:01 -0700

    DFS done

--- DFS.java
+++ DFS.java
@@ -1,3 +1,10 @@
 void DFS(Node root){
-	
+	if(root != null){
+		root.visited = true;
+		for(int i = 0; i < root.adjacent.size(); i++){
+			if(root.adjacent[i].visited == false){
+				DFS(root.adjacent[i]);
+			}
+		}
+	}
 }
