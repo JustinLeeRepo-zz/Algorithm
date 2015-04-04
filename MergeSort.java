8b2dfca38b94fa8fb10f5b1e875fa4eb824b072d
Justin Lee <justinj.lee23@gmail.com>
2015-04-03 21:46:02 -0700

    MergeSort full skeleton

--- MergeSort.java
+++ MergeSort.java
@@ -1,3 +1,8 @@
-public void merge(){
+public void mergeSort(int[] array){
+	int[] helper = new int[array.length];
+	mergeSort(array, helper, 0, array.length - 1);
+}
+
+public void mergeSort(int[] array, int[] helper, int low, int high){
 	
 }
