5d47bbee2d21520c3d8fcbc909115d65fc94b665
Justin Lee <justinj.lee23@gmail.com>
2015-04-06 19:55:36 -0700

    merge of mergeSort data types and helper array filled

--- MergeSort.java
+++ MergeSort.java
@@ -13,5 +13,11 @@ public void mergeSort(int[] array, int[] helper, int low, int high){
 }
 
 public void merge(int[] array, int[] helper, int low, int mid, int high){
+	for(int i = low; i <= high; i++){
+		helper[i] = array[i];
+	}
 
+	int helperLeft = low;
+	int helperRight = mid + 1;
+	int current = low;
 }
