18476d81c187dcfd8fc5f9b78f594182472835ac
Justin Lee <justinj.lee23@gmail.com>
2015-04-13 11:06:14 -0700

    duplicateFinder completed

--- duplicateFinder.py
+++ duplicateFinder.py
@@ -11,7 +11,30 @@ def find_duplicate_files(path):
 
             fr = open(filePath, 'r')
             #hash each readline of open file
-            for line in fr:                
-
+            for line in fr:
+                hashIndex = fileHasher(line)
+        
+                if (hashIndex in duplicates):
+                    #open and compare found hashed file with current file
+                    frCompare = open(duplicates[hashIndex][0], 'r')
+                    if (fr == frCompare):
+                        #duplicate found and add. continue with other files in current directory
+                        duplicates[hashIndex].append(filePath)
+                        frCompare.close()
+                        fr.close()
+                        break
+                    else:
+                        #hash next line of file
+                        frCompare.close()
+                else:
+                    #not hashed yet so add list with new file found. continue with other files in current directory
+                    duplicates[hashIndex] = [path]
+                    fr.close()
+                    break        
+    #finish traversing filesystem
+    #filter through duplicates that were not duplicates
+    for duplicate in duplicates:
+        if (duplicate.length == 1):
+            del duplicates[duplicate]
             
     return duplicates
