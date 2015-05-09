d436f70c5723acf2155eda0277f9bd5388576811
Justin Lee <justinj.lee23@gmail.com>
2015-05-08 21:14:54 -0700

    fix input parameter to be correct list type

--- Knapsack.java
+++ Knapsack.java
@@ -1,6 +1,6 @@
-public static int Knapsack(int w, Pair<Integer, Integer> items){
+public static int Knapsack(int w, List<Pair<Integer, Integer>> items){
 	int[] result = new int[w + 1];
-	for(Pair<Integer, Integer> item : items){
+	for(List<Pair<Integer, Integer>> item : items){
 		for(int j = w; j >= item.getFirst(); j--){
 			result[j] = max(result[j], result[j - item.getFirst()] + item.getSecond());
 		}
