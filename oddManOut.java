2b115cb037032a21446c20a2c81232d13ce34a85
Justin Lee <justinj.lee23@gmail.com>
2015-05-15 17:59:05 -0700

    if conditoin, lse needed

--- oddManOut.java
+++ oddManOut.java
@@ -2,7 +2,9 @@ public static int oddManOut(int[] arr){
 	Set<Integer> s = new HashSet<Integer>();
 	int sum = 0;
 	for(int i = 0; i < arr.length; i++){
-
+		if(s.conatains(arr[i])){
+			sum -= arr[i];
+		}
 	}
 	return ;
 }
