c3e681655c82c44cd6fbaa387c0e87ae526fcf06
Justin Lee <justinj.lee23@gmail.com>
2015-05-21 23:14:58 -0700

    data type declared and input parameter for compute fn

--- TreeDiameter.java
+++ TreeDiameter.java
@@ -1,9 +1,9 @@
 public static class TreeDiameter{
 	public static class  TreeNode{
-
+		List<Pair<TreeNode, Integer>> edges = new ArrayList<>();
 	}
 
-	public static Pair<double, double> computeDiamter(){
+	public static Pair<double, double> computeDiamter(TreeNode e){
 		
 	}
 }
