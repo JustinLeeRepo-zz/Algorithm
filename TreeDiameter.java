4573705597e439688964b2eb8ab704984aee4908
Justin Lee <justinj.lee23@gmail.com>
2015-05-23 09:01:44 -0700

    recursive call added

--- TreeDiameter.java
+++ TreeDiameter.java
@@ -7,7 +7,7 @@ public static class TreeDiameter{
 		double diamteter = Integer.MIN_VALUE;
 		double height = {.0, .0};
 		for(Pair<TreeNode, Integer> e : r.edges){
-			
+			Pair<Double, Double> heightDiam = computeDiamter(e.getFirst());
 		}
 
 
