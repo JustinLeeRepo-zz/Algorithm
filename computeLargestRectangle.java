82ee062a260f2a8b2eb8fd90859442dfc0d4f76d
Justin Lee <justinj.lee23@gmail.com>
2015-05-27 14:07:32 -0700

    return type, result, intialized to proper val

--- computeLargestRectangle.java
+++ computeLargestRectangle.java
@@ -1,6 +1,6 @@
 public static int computeLargestRectangle(List<Integer> A){
 	Stack<Integer> s = new Stack<>();
-	int result;
+	int result = Integer.MIN_VALUE;
 
 	for(int i = 0; i < A.size(); i++){
 		s.push(i);
