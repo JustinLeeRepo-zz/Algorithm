47e3576d7170d9bdb7084750e0c5efb51f91d3e1
Justin Lee <justinj.lee23@gmail.com>
2015-05-29 23:29:40 -0700

    call helper fn w/ parameters

--- permutation.java
+++ permutation.java
@@ -1,8 +1,9 @@
 public static List<List<Integer>> permutation(List<Integer> a){
 	List<List<integer>> result = new ArrayList<>();
+	permutationHeler(a, 0,result);
 	return result;
 }
 
 public static void permutationHelper(){
-	
+
 }
