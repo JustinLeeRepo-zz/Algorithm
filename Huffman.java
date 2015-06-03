3715df28a6c25b8688c26eb4027d135fee91a38f
Justin Lee <justinj.lee23@gmail.com>
2015-06-03 11:39:49 -0700

    compareTo fn done

--- Huffman.java
+++ Huffman.java
@@ -3,7 +3,7 @@ public static class Symbol{
 	public static class BTree extends Comparable<Btree>{
 		@override
 		public int compareTo(BTree b){
-			
+			return Double.compare(freq, b.freq);
 		}
 	}
 }
