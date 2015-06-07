b56f9eb40bc9a5868ef77202113ef3fd88ed7626
Justin Lee <justinj.lee23@gmail.com>
2015-06-07 13:31:09 -0700

    Huffman BTree constructor

--- Huffman.java
+++ Huffman.java
@@ -1,12 +1,18 @@
-public static class Symbol{
+static class Symbol{
 	public double freq;
 	public char s;
 	public String bitPattern;
 
 	public static class BTree extends Comparable<Btree>{
-		public double freq;
-		public Symbol s;
-		public BTree left, right;
+		double freq;
+		Symbol s;
+		BTree left, right;
+
+		public BTree(){
+			
+		}
+
+
 		@override
 		public int compareTo(BTree b){
 			return Double.compare(freq, b.freq);
