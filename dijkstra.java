6331123ba8cd9ccaefbcab9787a2473e0231f9e6
Justin Lee <justinj.lee23@gmail.com>
2015-06-23 20:41:05 -0700

    data types of vertex class

--- dijkstra.java
+++ dijkstra.java
@@ -1,6 +1,8 @@
 public static class Vertex implements Comparable<Vertex>{
+	Pair< Integer, Integer > edge = new Pair<>(Integer.MAX_VALUE, 0);
+	List< Pair< Vertex, Integer >> edges = new ArrayList<>();
 	@override
-	public int compareTo(Vertex v){
+		public int compareTo(Vertex v){
 		
 	}
 }
