430afb6e283f69b8148d4a4422eda17bad385c4e
Justin Lee <justinj.lee23@gmail.com>
2015-06-24 22:08:45 -0700

    previous vertex type and unique id data type

--- dijkstra.java
+++ dijkstra.java
@@ -1,8 +1,10 @@
 public static class Vertex implements Comparable<Vertex>{
 	Pair< Integer, Integer > edge = new Pair<>(Integer.MAX_VALUE, 0);
 	List< Pair< Vertex, Integer >> edges = new ArrayList<>();
+	int id;
+	Vertex previous = null;
 	@override
-		public int compareTo(Vertex v){
+	public int compareTo(Vertex v){
 		
 	}
 }
