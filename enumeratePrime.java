223c1d673078b686ebb8013ac3fcae591cc74ad9
Justin Lee <justinj.lee23@gmail.com>
2015-08-09 19:59:52 -0700

    isPrimes decl enumeratePrime

--- enumeratePrime.java
+++ enumeratePrime.java
@@ -2,4 +2,6 @@ public static List<Integer> enumeratePrime(int n){
 	int size = Math.floor(5 * (n-3)) + 1;
 	List<integer> result = new ArrayList<>();
 	result.add(2);
+
+	boolean[] isPrimes = new boolean[size]
 }
