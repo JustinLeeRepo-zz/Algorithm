d3baf09c78932ae40cf8e39994766fa5f605bc2c
Justin Lee <justinj.lee23@gmail.com>
2015-08-14 21:35:55 -0700

    ChainedIterator data instances

--- ChainedIterator.java
+++ ChainedIterator.java
@@ -1,5 +1,9 @@
 class ChainedIterator<E> implements Iterator<E>{
+	private Iterator<E> firstItr;
+	private Iterator<E> secondItr;
+	private Iterator<E> prev;
+
 	ChainedIterator(Iterator<E> first, Iterator<E> second){
-		
+
 	}
 }
