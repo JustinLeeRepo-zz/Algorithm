a4a0fc1bdfee31e883297a98408c210263e271f4
Justin Lee <justinj.lee23@gmail.com>
2015-08-15 22:47:05 -0700

    ChainedIterator data initialization constructor

--- ChainedIterator.java
+++ ChainedIterator.java
@@ -4,6 +4,7 @@ class ChainedIterator<E> implements Iterator<E>{
 	private Iterator<E> prev;
 
 	ChainedIterator(Iterator<E> first, Iterator<E> second){
-
+		firstItr = first;
+		secondItr = second;
 	}
 }
