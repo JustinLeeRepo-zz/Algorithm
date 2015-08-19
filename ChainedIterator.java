e2232df336b05ab8f36a11f9871cb726ea47bedb
Justin Lee <justinj.lee23@gmail.com>
2015-08-18 22:28:43 -0700

    hasNext chained iterator skeleton

--- ChainedIterator.java
+++ ChainedIterator.java
@@ -3,8 +3,14 @@ class ChainedIterator<E> implements Iterator<E>{
 	private Iterator<E> secondItr;
 	private Iterator<E> prev;
 
-	ChainedIterator(Iterator<E> first, Iterator<E> second){
+	public ChainedIterator(Iterator<E> first, Iterator<E> second){
 		firstItr = first;
 		secondItr = second;
+
+		prev = first;
+	}
+
+	public boolean hasNext(){
+		
 	}
 }
