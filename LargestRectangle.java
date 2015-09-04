010074c04cd72c63b2f784a0f74b1f923e720fb0
Justin Lee <justinj.lee23@gmail.com>
2015-09-03 20:10:08 -0700

    Largest Rectangle iterate through each rectangle and then compare each rectangle to each other rectangle in the iterations

--- LargestRectangle.java
+++ LargestRectangle.java
@@ -2,5 +2,11 @@ public class LargestRectangle{
 	public static void solve(int[] arr){
 		int maxRectangle = Integer.MIN_VALUE;
 		int runningSum = -1;
+
+		for(int i = 0; i < arr.length; i++){
+			for(int compareTo = 0; compareTo < arr.length; compareTo++){
+				
+			}
+		}
 	}
 }
